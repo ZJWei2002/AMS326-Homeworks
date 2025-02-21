90933bc2fc32cbd132714490c24f5627729b1a69
Zijun Wei <Zijun.wei.1@cs.stonybrook.edu>
2025-02-20 21:16:17 -0500

    Q1 finished.

--- HW1.py
+++ HW1.py
@@ -1,6 +1,9 @@
 import math
 import random
 
+
+# 1
+
 def f(x):
     return math.exp(-x ** 3) - x ** 4 - math.sin(x)
 
@@ -23,13 +26,15 @@ def bisection(fun = f, a = -1, b = 1, tol = 5e-5):
     if fun(b) == 0:
         return b
     if fun(a) * fun(b) > 0 or a >= b:
-        return "error"
+        raise RuntimeError
     
     i = 0 # counter for iterations
+    f = 0 # counter for flops
     while (b - a) / 2 > tol:
         i += 1
         c = (a + b) / 2
         print(f"Iteration #{i}: a = {round(a, 5)} b = {round(b, 5)} c = {round(c, 5)}")
+        f += 13 # 2 divisions + the operation below 11
         if fun(a) * fun(c) < 0:
             b = c
         elif fun(a) * fun(c) > 0:
@@ -37,41 +42,53 @@ def bisection(fun = f, a = -1, b = 1, tol = 5e-5):
         else:
             return c
     
-    return (a + b) / 2
+    return (a + b) / 2, i, f
 
 def newton(fun = f, fun_prime = f_prime, x0 = 0, tol = 5e-5, max = 100):
-    i = 0
-    print(f"Iteration #0: x0 = {round(x0, 5)}")
+    i = 0 # counter for iterations
+    f = 0 # counter for flops
     while i < max:
         i += 1
         x1 = x0 - fun(x0) / fun_prime(x0)
         print(f"Iteration #{i}: x0 = {round(x0, 5)} x1 = {round(x1, 5)}")
+        f += 17
         if abs(x1 - x0) < tol:
-            return x1
+            return x1, i, f
         x0 = x1
-    return "Failed"
+    raise RuntimeError
+
 
 def secant(fun  = f, x0 = -1, x1 = 1, tol = 5e-5, max = 100):
-    i = 0
+    i = 0 # counter for iterations
+    f = 0 # counter for flops
     print(f"Iteration #0: x0 = {round(x0, 5)} x1 = {round(x1, 5)}")
     while i < max:
         i += 1
         x2 = x1 - fun(x1) * (x1 - x0) / (fun(x1) - fun(x0)) 
         print(f"Iteration #{i}: x0 = {round(x0, 5)} x1 = {round(x1, 5)} x2 = {round(x2, 5)}")
+        f += 21
         if abs(x2 - x1) < tol:
-            return x2
+            return x2, i, f
         x0 = x1
         x1 = x2
-    return "Failed"
+    raise RuntimeError
 
 def monte_carlo(fun = f, a = 0.5, b = 0.75, tol = 5e-5, max = 10000):
-    i = 0
+    i = 0 # counter for iterations
+    f = 0 # counter for flops
     while i < max:
         i += 1
         x = random.uniform(a, b)
         print(f"Iteration #{i}: x = {round(x, 5)} f(x) = {round(fun(x), 5)}")
+        f += 6
         if abs(fun(x)) < tol:
-            return x
-    return "Failed"
+            return x, i, f
+    raise RuntimeError
 
-print(monte_carlo())
+root, iters, flops = bisection()
+print(f"root: {root}, number of iterations: {iters}, number of flops: {flops}")
+
+
+
+# 2
+
